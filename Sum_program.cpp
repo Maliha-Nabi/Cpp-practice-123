892504b36e73aaf058e9046f343c198f50fdde61
Maliha Nabi <umamanabi@gmail.com>
2025-12-23 23:06:16 +0500

    Sum of two numbers program
    A program which takes 2 numbers from user as an input and display the sum.

---
+++ Sum_program.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+int num1,num2;
+cout<<"Enter number 1: ";
+cin>>num1;
+cout<<"Enter number 2: ";
+cin>>num2;
+sum=num1+num2;
+cout<<"Sum of 2 numbers is: "<<sum;
+
+return 0;
+}
